534ace8438df1348d2e39d11e2f3314f629380be
hks199 <hksm199@gmail.com>
2026-04-08 16:59:43 +0530

    Update EMSP_URL and CPO_URL to use secure HTTPS endpoints

--- src/ocpi/credentials.js
+++ src/ocpi/credentials.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-let EMSP_URL = "http://ubc-ocpi-adaptor:6001/ocpi/2.2.1";
+let EMSP_URL = "https://uat-bpp-opci-adaptor.ubc.nbsl.org.in/ocpi/2.2.1";
 
-let CPO_URL = "http://ocpi-simulator:6002/ocpi/2.2.1";
+let CPO_URL = "https://uat-bpp-opci.ubc.nbsl.org.in/ocpi/2.2.1";
 
 router.post("/", (req, res) => {
     EMSP_URL = req.body.url;
